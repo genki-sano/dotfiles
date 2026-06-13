1861856d30a308fe14bc76017a765a5fac38721b
genki-sano <g.sano.2441@gmail.com>
2026-06-13 19:43:47 +0900

    Fix Home Manager common config syntax

--- .config/nix/home-manager/common.nix
+++ .config/nix/home-manager/common.nix
@@ -18,5 +18,5 @@
     ".config/oh-my-zsh/custom/themes/powerlevel10k".source = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k";
     ".config/oh-my-zsh/custom/plugins/zsh-autosuggestions".source = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
     ".config/oh-my-zsh/custom/plugins/zsh-syntax-highlighting".source = "${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting";
-  }
+  };
 }
